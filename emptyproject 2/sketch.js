ef94fcbf94cdb98a1c083e21f7197caf65285c97
bolan97 <501109698@qq.com>
2023-02-01 21:23:49 +0000

    final

--- emptyproject 2/sketch.js
+++ emptyproject 2/sketch.js
@@ -20,15 +20,16 @@ function draw() {
 }
 
 function drawGrid() {
-	background(0);
-	for (var x = 0; x < width; x += width / 10) {
-		for (var y = 0; y < height; y += height / 10) {
-			stroke(255);
-			strokeWeight(1);
-			line(x, 0, x, height);
-			line(0, y, width, y);
-		}
-	}
+	stroke(255);
+      for (let i=0; i<width; i+=cellSize)
+    {
+      
+		ftline(i, 0, i, height);
+    }
+    for (let j=0; j<height; j+=cellSize)
+    {
+      ftline(0, j, width, j);
+    } 
 }
 
 function drawLines() {
