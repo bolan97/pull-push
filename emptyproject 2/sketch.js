86840a18043eb7162ba59d19c32d071867b0755f
bolan97 <501109698@qq.com>
2023-02-01 20:55:29 +0000

    hi! no.1

---
+++ emptyproject 2/sketch.js
@@ -0,0 +1,23 @@
+// draw the base grid
+
+function setup() {
+  createCanvas(800,800);
+}
+
+
+function draw() {
+  drawGrid();
+ 
+}
+
+function drawGrid() {
+	background(0);
+	for (var x = 0; x < width; x += width / 10) {
+		for (var y = 0; y < height; y += height / 10) {
+			stroke(255);
+			strokeWeight(1);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+		}
+	}
+}
