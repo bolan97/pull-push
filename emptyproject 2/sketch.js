5a71e2fc8fcb66823386b9e6db1080b01abea9a1
xuebiao123 <2574535014@qq.com>
2023-02-01 21:09:46 +0000

    Change background

--- emptyproject 2/sketch.js
+++ emptyproject 2/sketch.js
@@ -11,7 +11,7 @@ function draw() {
 }
 
 function drawGrid() {
-	background(0);
+	background(255);
 	for (var x = 0; x < width; x += width / 10) {
 		for (var y = 0; y < height; y += height / 10) {
 			stroke(255);
