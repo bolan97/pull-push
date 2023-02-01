b6659a723a56ddffef25016fc57135a39dca8dec
bolan97 <501109698@qq.com>
2023-02-01 22:25:29 +0000

    style test

--- emptyproject 2/sketch.js
+++ emptyproject 2/sketch.js
@@ -144,3 +144,15 @@ function ftline(x1,y1,x2,y2 ) {
 	vertex( x2 + random(-3,3), y2 +random(-3,3));
 	endShape();  
   }
+  
+  // function ftline(x1,y1,x2,y2 ) {
+  //   noFill();
+  //   beginShape();
+  //   vertex( x1 + random(-30,30), y1 +random(-30,30));
+  //   curveVertex( x1 + random(-30,30), y1 +random(-30,30));
+  //   curveVertex( x1+(x2 -x1)/3 + random(-30,30), y1 + (y2-y1)/3 +random(-30,30));
+  //   curveVertex( x1+2*(x2-x1)/3 + random(-30,30), y1+ 2*(y2-y1)/3 +random(-30,30)); 
+  //   curveVertex( x2 + random(-30,30), y2 +random(-30,30));
+  //   vertex( x2 + random(-30,30), y2 +random(-30,30));
+  //   endShape();  
+  //   }
